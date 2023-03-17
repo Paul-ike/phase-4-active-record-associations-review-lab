a2046e5a699b446a824dfde3e225ec1f9c173477
Paul <paul.ngugi@student.moringaschool.com>
2023-03-17 16:57:35 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+taxi1 = Taxi.create(name: Faker::Lorem.word)
+taxi2 = Taxi.create(name: Faker::Lorem.word)
+taxi3 = Taxi.create(name: Faker::Lorem.word)
+taxi4 = Taxi.create(name: Faker::Lorem.word)
+
+passenger1 = Passenger.create(name: Faker::Lorem.word)
+passenger2 = Passenger.create(name: Faker::Lorem.word)
+passenger3 = Passenger.create(name: Faker::Lorem.word)
+
+Ride.create(taxi_id: taxi1.id, passenger_id: passenger1.id)
+Ride.create(taxi_id: taxi1.id, passenger_id: passenger2.id)
+
+Ride.create(taxi_id: taxi2.id, passenger_id: passenger1.id)
+Ride.create(taxi_id: taxi2.id, passenger_id: passenger3.id)
+
+Ride.create(taxi_id: taxi3.id, passenger_id: passenger2.id)
+
+Ride.create(taxi_id: taxi4.id, passenger_id: passenger2.id)
+Ride.create(taxi_id: taxi4.id, passenger_id: passenger3.id)
